834b02e2f55eab969856daab490541d64b47cc5a
M.Kozak <kozak_m@outlook.com>
2019-07-02 18:23:05 +0200

    implementingTheRoute

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const feedRoutes = require('./routes/feed')
+
+const app = express();
+
+app.listen(8080);
---
+++ controllers/feed.js
@@ -0,0 +1,3 @@
+exports.getPosts = (req,res,next) => {
+
+}
