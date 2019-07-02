ea047ee0ade00feab74f492d6aebe30daa01894d
M.Kozak <kozak_m@outlook.com>
2019-07-02 19:10:15 +0200

    sendingRequest&ResponsesAndWorkingWithPostman

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed')
 
 const app = express();
+
+//app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+app.use('/feed', feedRoutes)
 
 app.listen(8080);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,22 @@
 exports.getPosts = (req,res,next) => {
+    res.status(200).json({
+        posts: [{
+            title: 'First Page',
+            content: 'This is the first post!'
+        }]
+    });
+}
 
+exports.createPost = (req,res,next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+    
+    res.status(201).json({
+        message: 'Post created successfuly!',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content
+        }
+    });
 }
