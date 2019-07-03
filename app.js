9373dfb524d114ab6973df34e3849df3826991d3
M.Kozak <kozak_m@outlook.com>
2019-07-03 17:55:22 +0200

    fetchingListsofPosts

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin'), '*';
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Methods', 'Content-Type, Authorization');
     next();
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,8 +1,14 @@
 exports.getPosts = (req,res,next) => {
     res.status(200).json({
         posts: [{
+            _id: '1',
             title: 'First Page',
-            content: 'This is the first post!'
+            content: 'This is the first post!',
+            imageUrl: 'images/duck.jpg',
+            creator: {
+                name: 'Iza'
+            },
+            createdAt: new Date()
         }]
     });
 }
