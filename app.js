751a5dac3bc45ca713f55ffd7406f4e5e88a4884
M.Kozak <kozak_m@outlook.com>
2019-07-04 17:46:24 +0200

    modelPostSchema

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed')
 
@@ -18,5 +19,10 @@ app.use((req,res,next)=>{
 
 app.use('/feed', feedRoutes)
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://kozak:53352190@cluster0-ypi1l.mongodb.net/shop').then(result=>{
+    app.listen(8080);
+}).catch(err=>{
+    console.log(err)
+})
+
 
