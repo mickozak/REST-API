8764510c170908f325c53ab3ae160ff483f07a56
M.Kozak <kozak_m@outlook.com>
2019-07-05 19:56:29 +0200

    modelsUser

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const multer = require('multer');
 
 
 const feedRoutes = require('./routes/feed')
+const authRoutes = require('./routes/auth')
+
 
 const app = express();
 
@@ -56,6 +58,7 @@ app.use((req,res,next)=>{
 });
 
 app.use('/feed', feedRoutes)
+app.use('/auth', authRoutes)
 
 app.use((error, req, res, next)=>{
     const status = error.statusCode || 500;
