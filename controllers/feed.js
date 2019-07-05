e8af676869496125967658ffd71a21a48bd0d2ca
M.Kozak <kozak_m@outlook.com>
2019-07-05 19:47:32 +0200

    pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,20 +5,34 @@ const { validationResult } = require('express-validator/check')
 
 const Post = require('../models/post')
 
-exports.getPosts = (req,res,next) => {
+exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
-    .then(posts=>{
-        res.status(200).json({
-            message: 'Fetchied post successfully',
-            posts: posts
-        })
-    }).catch(err=>{
-        if(!err.statusCode){
-            err.statusCode = 500;
+      .countDocuments()
+      .then(count => {
+        totalItems = count;
+        return Post.find()
+          .skip((currentPage - 1) * perPage)
+          .limit(perPage);
+      })
+      .then(posts => {
+        res
+          .status(200)
+          .json({
+            message: 'Fetched posts successfully.',
+            posts: posts,
+            totalItems: totalItems
+          });
+      })
+      .catch(err => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
         }
         next(err);
-    })
-}
+      });
+  };
 
 exports.createPost = (req,res,next) => {
     const errors = validationResult(req)
@@ -161,3 +175,5 @@ exports.updatePost = (req, res, next) => {
     fs.unlink(filePath, err => console.log(err));
   };
   
+
+
