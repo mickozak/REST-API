44d1dba3bd1a00c9fce9b5a19878fefb870a3d4b
M.Kozak <kozak_m@outlook.com>
2019-07-06 17:57:04 +0200

    addingAuthMiddlewareToAllRoutes

--- routes/feed.js
+++ routes/feed.js
@@ -8,19 +8,19 @@ const router = express.Router();
 
 router.get('/posts', isAuth, feedController.getPosts);
 
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({min: 7}),
     body('content').trim().isLength({min: 5})
 ], feedController.createPost);
 
-router.get('/post/:postId', feedController.getPost)
+router.get('/post/:postId', isAuth,feedController.getPost)
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth,[
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.updatePost);
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth,feedController.deletePost);
 
 module.exports = router;
 
