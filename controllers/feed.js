8eb51c4a5eb6e13bc4970bb9716461e25a7a5582
M.Kozak <kozak_m@outlook.com>
2019-07-08 20:22:03 +0200

    updatingPostsOnAllConnectedClients

--- controllers/feed.js
+++ controllers/feed.js
@@ -111,13 +111,13 @@ exports.updatePost = async (req, res, next) => {
     throw error;
   }
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate('creator');
     if (!post) {
       const error = new Error('Could not find post.');
       error.statusCode = 404;
       throw error;
     }
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized!');
       error.statusCode = 403;
       throw error;
@@ -129,6 +129,7 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl;
     post.content = content;
     const result = await post.save();
+    io.getIO().emit('posts', {action: 'update', post: result});
     res.status(200).json({ message: 'Post updated!', post: result });
   } catch (err) {
     if (!err.statusCode) {
