8e38f0977b6fe22db25341043d11c7657d9bce5e
M.Kozak <kozak_m@outlook.com>
2019-07-08 20:36:19 +0200

    deletingPostAcrossClients

--- controllers/feed.js
+++ controllers/feed.js
@@ -14,6 +14,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({createdAt: - 1})
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -161,7 +162,7 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
-
+    io.getIO().emit('posts', {action: 'delete', post: postId}). 
     res.status(200).json({ message: 'Deleted post.' });
   } catch (err) {
     if (!err.statusCode) {
